8985327299f3b85f9acf2301cf04cf51e9672b7b
Alex Schwartz <alexschwartz01@gmail.com>
2025-06-28 14:02:56 -0400

    arch detection and cleanup

--- nodeswitch.sh
+++ nodeswitch.sh
@@ -1,71 +1,66 @@
-LOCAL_SHARE="$HOME/.local/share"
+nodeswitchLocalShare="$HOME/.local/share"
+nodeswitchUnameM=$(uname -m)
 
-if [ "$1" != "" ]
-then
-   if [ "$2" != "" ]
-   then
-       if [ "$3" != "" ]
-       then
+if [ "$nodeswitchUnameM" = "x86_64" ]; then
+   nodeswitchArch=x64
+elif [ "$nodeswitchUnameM" = "aarch64" ]; then
+   nodeswitchArch=arm64
+else
+   echo "ARCH not supported"
+   exit
+fi
+
+if [ "$1" != "" ]; then
+   if [ "$2" != "" ]; then
+       if [ "$3" != "" ]; then
            echo "Incorrect command"
        else
-           if [ "$1" = "remove" ]
-           then
-               if [ -d "$LOCAL_SHARE/nodeswitch/$2" ]
-               then
-                   rm -rf "$LOCAL_SHARE/nodeswitch/$2"
+           if [ "$1" = "remove" ]; then
+               if [ -d "$nodeswitchLocalShare/nodeswitch/$2" ]; then
+                   rm -rf "$nodeswitchLocalShare/nodeswitch/$2"
                else
                    echo "Node version not installed"
                fi
-           elif [ "$1" = "use" ]
-           then
-               if [ "$2" = "default" ]
-               then
-                   if [ ! -z "$nodeswitchDefaultPATH" ]
-                   then
+           elif [ "$1" = "use" ]; then
+               if [ "$2" = "default" ]; then
+                   if [ ! -z "$nodeswitchDefaultPATH" ]; then
                        export PATH="$nodeswitchDefaultPATH"
                    fi
                else
-                   if [ -d "$LOCAL_SHARE/nodeswitch/$2" ]
-                   then
-                       if [ -z "$nodeswitchDefaultPATH" ]
-                       then
+                   if [ -d "$nodeswitchLocalShare/nodeswitch/$2" ]; then
+                       if [ -z "$nodeswitchDefaultPATH" ]; then
                            export nodeswitchDefaultPATH="$PATH"
-                           export PATH="$LOCAL_SHARE/nodeswitch/$2/bin:$PATH"
+                           export PATH="$nodeswitchLocalShare/nodeswitch/$2/bin:$PATH"
                        else
-                           export PATH="$LOCAL_SHARE/nodeswitch/$2/bin:$nodeswitchDefaultPATH"
+                           export PATH="$nodeswitchLocalShare/nodeswitch/$2/bin:$nodeswitchDefaultPATH"
                        fi
                    else
                        echo "Node version not installed"
                    fi
                fi
-           elif [ "$1" = "add" ]
-           then
-               if [ ! -d "$LOCAL_SHARE/nodeswitch/$2" ]
-               then
+           elif [ "$1" = "add" ]; then
+               if [ ! -d "$nodeswitchLocalShare/nodeswitch/$2" ]; then
                    curl -f https://nodejs.org/download/release/v$2/ &>/dev/null
-                   if [ $? -eq 0 ]
-                   then
-                       curl -s -o "$LOCAL_SHARE/nodeswitch/$2.xz" https://nodejs.org/download/release/v$2/node-v$2-linux-x64.tar.xz > /dev/null
+                   if [ $? -eq 0 ]; then
+                       curl -s -o "$nodeswitchLocalShare/nodeswitch/$2.xz" https://nodejs.org/download/release/v$2/node-v$2-linux-$nodeswitchArch.tar.xz > /dev/null
 
-                       if [ $? -ne 0 ]
-                       then
-                           rm "$LOCAL_SHARE/nodeswitch/$2.xz"
+                       if [ $? -ne 0 ]; then
+                           rm "$nodeswitchLocalShare/nodeswitch/$2.xz"
                            echo "Node version not created"
                            exit
                        fi
 
-                       tar -xf "$LOCAL_SHARE/nodeswitch/$2.xz" -C "$LOCAL_SHARE/nodeswitch" > /dev/null
+                       tar -xf "$nodeswitchLocalShare/nodeswitch/$2.xz" -C "$nodeswitchLocalShare/nodeswitch" > /dev/null
 
-                       if [ $? -ne 0 ]
-                       then
-                           rm "$LOCAL_SHARE/nodeswitch/$2.xz"
-                           rm -rf "$LOCAL_SHARE/nodeswitch/node-v$2-linux-x64"
+                       if [ $? -ne 0 ]; then
+                           rm "$nodeswitchLocalShare/nodeswitch/$2.xz"
+                           rm -rf "$nodeswitchLocalShare/nodeswitch/node-v$2-linux-$nodeswitchArch"
                            echo "Node version not created"
                            exit
                        fi
 
-                       rm "$LOCAL_SHARE/nodeswitch/$2.xz"
-                       mv "$LOCAL_SHARE/nodeswitch/node-v$2-linux-x64" "$LOCAL_SHARE/nodeswitch/$2" > /dev/null
+                       rm "$nodeswitchLocalShare/nodeswitch/$2.xz"
+                       mv "$nodeswitchLocalShare/nodeswitch/node-v$2-linux-$nodeswitchArch" "$nodeswitchLocalShare/nodeswitch/$2" > /dev/null
                    else
                        echo "Node version not found"
                    fi
@@ -77,12 +72,10 @@ then
            fi
        fi
    else
-       if [ "$1" = "list" ]
-       then
-           dir -1 "$LOCAL_SHARE/nodeswitch"
-       elif [ "$1" = "path" ]
-       then
-           echo "$LOCAL_SHARE/nodeswitch"
+       if [ "$1" = "list" ]; then
+           dir -1 "$nodeswitchLocalShare/nodeswitch"
+       elif [ "$1" = "path" ]; then
+           echo "$nodeswitchLocalShare/nodeswitch"
        else
            echo "Incorrect command"
        fi
