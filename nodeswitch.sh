a965b1ce9c1888f0840a77ef5aa20010620f3b47
Alex Schwartz <alexschwartz01@gmail.com>
2026-09-09 15:43:50 -0400

    oops, lib prefix

--- nodeswitch.sh
+++ nodeswitch.sh
@@ -63,7 +63,7 @@ if [ "$1" != "" ]; then
                         mv "$nodeswitchLocalShare/nodeswitch/node-v$2-linux-$nodeswitchArch" "$nodeswitchLocalShare/nodeswitch/$2"
 
                         mkdir -p "$nodeswitchLocalShare/nodeswitch/$2/globalPrefix/lib"
-                        echo "prefix=$nodeswitchLocalShare/nodeswitch/$2/globalPrefix" > "$nodeswitchLocalShare/nodeswitch/$2/node_modules/npm/npmrc"
+                        echo "prefix=$nodeswitchLocalShare/nodeswitch/$2/globalPrefix" > "$nodeswitchLocalShare/nodeswitch/$2/lib/node_modules/npm/npmrc"
                     else
                         echo "Node version not found"
                     fi
